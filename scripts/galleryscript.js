27edbe8ad38106ca2baba062b63c02d6c38f1efa
Emily <eychow@sfu.ca>
2020-04-09 02:26:34 -0700

    file extension, vfx content, index hover
    replaced all file extensions to the right ones ex. png > PNG
    
    added content to vfx and images
    vfx now has working gallery
    vfx gallery lightbox needs captions
    
    when hovering over gallery items in index, the cube changes to a gif

---
+++ scripts/galleryscript.js
@@ -0,0 +1,9 @@
+// ONHOVER CHANGE IMAGE for gallery https://stackoverflow.com/questions/10886828/changing-image-on-hover
+function onHover() {
+  // document.getElementById("#changeImg").attr('src', 'images/thecubegif.gif');
+  document.getElementById("changeImg").src = "images/thecubegif.gif";
+}
+
+function offHover() {
+  document.getElementById("changeImg").src = 'images/thecube.PNG';
+}
